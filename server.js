b39bd2e02b729e46d71265cc9a24f87543e20316
jaykee28 <jmufetu@gmail.com>
2026-05-06 00:05:23 +0200

    Completed W01 Express and EJS templating activity

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const app = express();
+// Define environment
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Define port
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+
+// Set views directory
+app.set('views', path.join(__dirname, 'src/views'));
+
+app.get('/', (req, res) => {
+  res.render('home', { title: 'Home' });
+});
+
+app.get('/organizations', (req, res) => {
+  res.render('organizations', { title: 'Organizations' });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects', { title: 'Service Projects' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+  console.log(`Environment: ${NODE_ENV}`);
+});
